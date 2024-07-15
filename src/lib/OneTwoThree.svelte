edc95572523d675bc6f9441ac4d80e616cc1b77e
John <curran.john.m@gmail.com>
2024-07-14 21:34:09 -0400

    Needs heavy revision

---
+++ src/lib/OneTwoThree.svelte
@@ -0,0 +1,51 @@
+<section class="px-8 py-16 bg-neutral">
+    <div class="max-w-7xl mx-auto">
+      <div class="flex flex-wrap -mx-6">
+        <div class="w-full md:w-1/3 px-6 mb-12">
+          <article class="group">
+            <img
+              alt=""
+              src="/agenda.webp"
+              class="h-56 w-full rounded-xl object-cover shadow-xl transition group-hover:grayscale-[50%]"
+            />
+            <div class="p-4">
+              <h3 class="text-xl font-medium text-base-100 font-raleway">Invites with Agenda</h3>
+              <p class="mt-2 line-clamp-3 text-md/relaxed text-base-100 font-overpass">
+                The meeting leader makes sure everybody is involved, and lists a few things they want people to cover, like usual.
+              </p>
+            </div>
+          </article>
+        </div>
+        <div class="w-full md:w-1/3 px-6 mb-12">
+          <article class="group">
+            <img
+              alt=""
+              src="/record.webp"
+              class="h-56 w-full rounded-xl object-cover shadow-xl transition group-hover:grayscale-[50%]"
+            />
+            <div class="p-4">
+              <h3 class="text-xl font-medium text-base-100 font-raleway">Record Your Part and Relax</h3>
+              <p class="mt-2 line-clamp-3 text-md/relaxed text-base-100 font-overpass">
+                No need to get nervous with MeetingMesh. Just record what you want to say from the agenda and upload it.
+              </p>
+            </div>
+          </article>
+        </div>
+        <div class="w-full md:w-1/3 px-6 mb-12">
+          <article class="group">
+            <img
+              alt=""
+              src="/success.webp"
+              class="h-56 w-full rounded-xl object-cover shadow-xl transition group-hover:grayscale-[50%]"
+            />
+            <div class="p-4">
+              <h3 class="text-xl font-medium text-base-100 font-raleway">Enjoy the Full Report</h3>
+              <p class="mt-2 line-clamp-3 text-md/relaxed text-base-100 font-overpass">
+                So it's really two steps. Our in-house AI stitches together all of the recordings, then gives you a summary.
+              </p>
+            </div>
+          </article>
+        </div>
+      </div>
+    </div>
+  </section>
---
+++ src/lib/USP.svelte
@@ -0,0 +1,47 @@
+<section class="bg-neutral text-base-100 text-center">
+    <div class="max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16 mx-auto">
+      <div class="max-w-xl mx-auto">
+        <h2 class="text-3xl font-bold font-ralewaysm:text-4xl">Bringing Everyone Up to Speed</h2>
+        <p class="mt-4 text-base-100 font-overpass text-md">
+          Wasteful meetings cost everyone and nailing down mutually agreeable times can be tough. MeetingMesh goes beyond simple AI analysis to provide you with an asynchronous way to keep your team up-to-date.
+        </p>
+      </div>
+  
+      <div class="mt-8 grid grid-cols-1 gap-8 md:mt-16 md:grid-cols-2 md:gap-12 lg:grid-cols-3">
+        <div class="flex flex-col items-center gap-4">
+          <span class="shrink-0 rounded-lg bg-neutral p-4">
+            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#e2c02b" viewBox="0 0 256 256"><path d="M243.28,68.24l-24-23.56a16,16,0,0,0-22.59,0L104,136.23l-36.69-35.6a16,16,0,0,0-22.58.05l-24,24a16,16,0,0,0,0,22.61l71.62,72a16,16,0,0,0,22.63,0L243.33,90.91A16,16,0,0,0,243.28,68.24ZM103.62,208,32,136l24-24a.6.6,0,0,1,.08.08l42.35,41.09a8,8,0,0,0,11.19,0L208.06,56,232,79.6Z"></path></svg>
+          </span>
+          <div class="text-center">
+            <h2 class="text-lg font-bold font-raleway">Ease of Use</h2>
+            <p class="mt-1 text-sm text-overpass text-base-100">
+              Get the same features you would in other AI-enabled meeting summaries without having to set up anything each time you start the meeting
+            </p>
+          </div>
+        </div>
+  
+        <div class="flex flex-col items-center gap-4">
+          <span class="shrink-0 rounded-lg bg-neutral p-4">
+            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#e2c02b" viewBox="0 0 256 256"><path d="M243.28,68.24l-24-23.56a16,16,0,0,0-22.59,0L104,136.23l-36.69-35.6a16,16,0,0,0-22.58.05l-24,24a16,16,0,0,0,0,22.61l71.62,72a16,16,0,0,0,22.63,0L243.33,90.91A16,16,0,0,0,243.28,68.24ZM103.62,208,32,136l24-24a.6.6,0,0,1,.08.08l42.35,41.09a8,8,0,0,0,11.19,0L208.06,56,232,79.6Z"></path></svg>
+          </span>
+            <div class="text-center">
+            <h2 class="text-lg font-bold">Secure Access</h2>
+            <p class="mt-1 text-sm text-base-100">
+              All processing is done on MeetingMesh servers with API calls to trusted vendors. For specific compliance purposes, analysis can also be done on HIPAA-compliant platforms.
+            </p>
+          </div>
+        </div>
+  
+        <div class="flex flex-col items-center gap-4">
+          <span class="shrink-0 rounded-lg bg-neutral p-4">
+            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#e2c02b" viewBox="0 0 256 256"><path d="M243.28,68.24l-24-23.56a16,16,0,0,0-22.59,0L104,136.23l-36.69-35.6a16,16,0,0,0-22.58.05l-24,24a16,16,0,0,0,0,22.61l71.62,72a16,16,0,0,0,22.63,0L243.33,90.91A16,16,0,0,0,243.28,68.24ZM103.62,208,32,136l24-24a.6.6,0,0,1,.08.08l42.35,41.09a8,8,0,0,0,11.19,0L208.06,56,232,79.6Z"></path></svg>
+          </span>
+          <div class="text-center">
+            <h2 class="text-lg font-bold">Flexible Implementation</h2>
+            <p class="mt-1 text-sm text-base-100">
+              Use MeetingMesh like our competitors for a single recorded meeting with multiple parties. Our AI models handle speaker selection and can provide similar reports.
+            </p>
+          </div>
+        </div>
+  
+  </section>
